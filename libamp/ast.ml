6b8a89948bcd5613d4615b66a6ab89d5ac60668e
Joongwon Ahn <freleefty@gmail.com>
2026-05-02 12:10:59 +0900

    Refactor AST nodes to use located payloads

--- libamp/ast.ml
+++ libamp/ast.ml
@@ -1,30 +1,46 @@
 type loc = { from : int; to_ : int }
 
-type exp =
-  | Num of { n : int; loc : loc }
-  | Var of { name : string; loc : loc }
-  | Ctor of { tag : string; args : exp array; loc : loc }
-  | Prim of { op : string; args : exp array; loc : loc }
+type 'a located = { payload : 'a; loc : loc }
 
-type cmd =
-  | Return of { label : int; exp : exp; loc : loc }
-  | Let of { label : int; x : string; exp : exp; body : cmd; loc : loc }
-  | LetCall of {
-      label : int;
-      x : string;
-      fn : string;
-      args : exp array;
-      body : cmd;
-      loc : loc;
-    }
-  | Match of {
-      label : int;
-      scrutinee : exp;
-      branches : branch array;
-      loc : loc;
-    }
+[@@@warning "-30"]
+
+type exp =
+  | Num of num_payload located
+  | Var of var_payload located
+  | Ctor of app_payload located
+  | Prim of prim_payload located
+
+and cmd =
+  | Return of return_payload located
+  | Let of let_payload located
+  | LetCall of let_call_payload located
+  | Match of match_payload located
 
 and branch = { tag : string; vars : string array; body : cmd; loc : loc }
+
+and num_payload = { n : int }
+
+and var_payload = { name : string }
+
+and app_payload = { tag : string; args : exp array }
+
+and prim_payload = { op : string; args : exp array }
+
+and return_payload = { label : int; exp : exp }
+
+and let_payload = { label : int; x : string; exp : exp; body : cmd }
+
+and let_call_payload = {
+  label : int;
+  x : string;
+  fn : string;
+  args : exp array;
+  body : cmd;
+}
+
+and match_payload = { label : int; scrutinee : exp; branches : branch array }
+
+[@@@warning "+30"]
 
 type def = { name : string; params : string array; body : cmd; loc : loc }
 
@@ -33,23 +49,6 @@ type prog = {
   mainName : string;
   ctrl : cmd Js.Dict.t;
 }
-
-type num_payload = { n : int }
-type var_payload = { name : string }
-type app_payload = { tag : string; args : exp array }
-type prim_payload = { op : string; args : exp array }
-type return_payload = { label : int; exp : exp }
-type let_payload = { label : int; x : string; exp : exp; body : cmd }
-
-type let_call_payload = {
-  label : int;
-  x : string;
-  fn : string;
-  args : exp array;
-  body : cmd;
-}
-
-type match_payload = { label : int; scrutinee : exp; branches : branch array }
 
 type 'a exp_visitor = {
   num : num_payload -> loc -> 'a;
@@ -65,88 +64,58 @@ type 'a cmd_visitor = {
   match_ : match_payload -> loc -> 'a;
 }
 
-let num (payload : num_payload) loc = Num { n = payload.n; loc }
-let var_ (payload : var_payload) loc = Var { name = payload.name; loc }
-let ctor (payload : app_payload) loc = Ctor { tag = payload.tag; args = payload.args; loc }
-let prim (payload : prim_payload) loc = Prim { op = payload.op; args = payload.args; loc }
-
-let return_ (payload : return_payload) loc =
-  Return { label = payload.label; exp = payload.exp; loc }
-
-let let_ (payload : let_payload) loc =
-  Let
-    {
-      label = payload.label;
-      x = payload.x;
-      exp = payload.exp;
-      body = payload.body;
-      loc;
-    }
-
-let letCall (payload : let_call_payload) loc =
-  LetCall
-    {
-      label = payload.label;
-      x = payload.x;
-      fn = payload.fn;
-      args = payload.args;
-      body = payload.body;
-      loc;
-    }
-
-let match_ (payload : match_payload) loc =
-  Match
-    {
-      label = payload.label;
-      scrutinee = payload.scrutinee;
-      branches = payload.branches;
-      loc;
-    }
+let num (payload : num_payload) loc = Num { payload; loc }
+let var_ (payload : var_payload) loc = Var { payload; loc }
+let ctor (payload : app_payload) loc = Ctor { payload; loc }
+let prim (payload : prim_payload) loc = Prim { payload; loc }
+let return_ (payload : return_payload) loc = Return { payload; loc }
+let let_ (payload : let_payload) loc = Let { payload; loc }
+let letCall (payload : let_call_payload) loc = LetCall { payload; loc }
+let match_ (payload : match_payload) loc = Match { payload; loc }
 
 let cmdLabel = function
-  | Return { label; _ }
-  | Let { label; _ }
-  | LetCall { label; _ }
-  | Match { label; _ } ->
-      label
+  | Return { payload; _ } -> payload.label
+  | Let { payload; _ } -> payload.label
+  | LetCall { payload; _ } -> payload.label
+  | Match { payload; _ } -> payload.label
 
 let cmdLoc = function
-  | Return { loc; _ } | Let { loc; _ } | LetCall { loc; _ } | Match { loc; _ }
-    ->
-      loc
+  | Return { loc; _ } -> loc
+  | Let { loc; _ } -> loc
+  | LetCall { loc; _ } -> loc
+  | Match { loc; _ } -> loc
 
 let withExp e visitor =
   match e with
-  | Num { n; loc } -> visitor.num { n } loc
-  | Var { name; loc } -> visitor.var { name } loc
-  | Ctor { tag; args; loc } -> visitor.ctor { tag; args } loc
-  | Prim { op; args; loc } -> visitor.prim { op; args } loc
+  | Num { payload; loc } -> visitor.num payload loc
+  | Var { payload; loc } -> visitor.var payload loc
+  | Ctor { payload; loc } -> visitor.ctor payload loc
+  | Prim { payload; loc } -> visitor.prim payload loc
 
 let withCmd c visitor =
   match c with
-  | Return { label; exp; loc } -> visitor.return { label; exp } loc
-  | Let { label; x; exp; body; loc } ->
-      visitor.let_ { label; x; exp; body } loc
-  | LetCall { label; x; fn; args; body; loc } ->
-      visitor.letCall { label; x; fn; args; body } loc
-  | Match { label; scrutinee; branches; loc } ->
-      visitor.match_ { label; scrutinee; branches } loc
+  | Return { payload; loc } -> visitor.return payload loc
+  | Let { payload; loc } -> visitor.let_ payload loc
+  | LetCall { payload; loc } -> visitor.letCall payload loc
+  | Match { payload; loc } -> visitor.match_ payload loc
 
 let rec expSummary e =
   match e with
-  | Num { n; _ } -> string_of_int n
-  | Var { name; _ } -> name
-  | Ctor { tag; args; _ } ->
+  | Num { payload = { n }; _ } -> string_of_int n
+  | Var { payload = { name }; _ } -> name
+  | Ctor { payload = { tag; args }; _ } ->
       tag ^ "(" ^ (args |> Array.map expSummary |> Array.to_list |> String.concat ", ") ^ ")"
-  | Prim { op; args; _ } ->
+  | Prim { payload = { op; args }; _ } ->
       op ^ "(" ^ (args |> Array.map expSummary |> Array.to_list |> String.concat ", ") ^ ")"
 
 let cmdSummary c =
   match c with
-  | Return { exp; _ } -> "return " ^ expSummary exp
-  | Let { x; exp; _ } -> "let " ^ x ^ " = " ^ expSummary exp ^ " in ..."
-  | LetCall { x; fn; args; _ } ->
+  | Return { payload = { exp; _ }; _ } -> "return " ^ expSummary exp
+  | Let { payload = { x; exp; _ }; _ } ->
+      "let " ^ x ^ " = " ^ expSummary exp ^ " in ..."
+  | LetCall { payload = { x; fn; args; _ }; _ } ->
       "let " ^ x ^ " = " ^ fn ^ "("
       ^ (args |> Array.map expSummary |> Array.to_list |> String.concat ", ")
       ^ ") in ..."
-  | Match { scrutinee; _ } -> "match " ^ expSummary scrutinee ^ " with ..."
+  | Match { payload = { scrutinee; _ }; _ } ->
+      "match " ^ expSummary scrutinee ^ " with ..."
